8c70a7d7e9e536e54fe6588337b6f048973c2f44
biner1 <bineromer19@gmail.com>
2022-10-14 16:17:24 +0300

    Add user types classes

---
+++ src/main/java/main.java
@@ -0,0 +1,12 @@
+package main.java;
+
+import main.java.view.LoginView;
+
+public class main {
+    public static void main(String[] args) {
+
+        LoginView start= new LoginView();
+        start.login();
+
+    }
+}
