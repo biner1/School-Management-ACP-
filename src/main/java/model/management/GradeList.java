bef398cf55056aae00e772d57ef62c7dd338e213
abdullah-19-8 <abdullaahmadsheikh@gmail.com>
2022-10-21 18:21:37 +0300

    gradeList and TimeTableList done

--- src/main/java/model/management/GradeList.java
+++ src/main/java/model/management/GradeList.java
@@ -10,6 +10,19 @@ public class GradeList {
 
     public GradeList(){
         grades = new ArrayList<Grade>();
+
+        grades.add(new Grade(1,"1",2022));
+        grades.add(new Grade(2,"2",2021));
+        grades.add(new Grade(3,"3",2020));
+        grades.add(new Grade(4,"4",2019));
+        grades.add(new Grade(5,"5",2018));
+        grades.add(new Grade(6,"6",2017));
+        grades.add(new Grade(7,"7",2016));
+        grades.add(new Grade(8,"8",2015));
+        grades.add(new Grade(9,"9",2014));
+        grades.add(new Grade(10,"10",2013));
+        grades.add(new Grade(11,"11",2012));
+        grades.add(new Grade(12,"12",2011));
     }
 
     public void addGrade(Grade grade){
--- src/main/java/model/models/TimeTable.java
+++ src/main/java/model/models/TimeTable.java
@@ -14,6 +14,7 @@ public class TimeTable {
     }
 
 
+
     public int getGradeId(){
         return gradeId;
     }
