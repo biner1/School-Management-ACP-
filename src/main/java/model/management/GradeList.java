dde24e3476be7b32037b128d35d8ba3028a0243e
abdullah-19-8 <abdullaahmadsheikh@gmail.com>
2022-10-21 19:55:34 +0300

    gradlist fixed

--- src/main/java/model/management/GradeList.java
+++ src/main/java/model/management/GradeList.java
@@ -13,11 +13,17 @@ public class GradeList {
         grades = new ArrayList<Grade>();
 
         grades.add(new Grade(1,"1A",2022));
-        grades.add(new Grade(2,"2A",2022));
-        grades.add(new Grade(3,"3A",2022));
-        grades.add(new Grade(4,"4A",2022));
-        grades.add(new Grade(5,"5A",2022));
-        grades.add(new Grade(6,"6A",2022));
+        grades.add(new Grade(2,"1B",2022));
+        grades.add(new Grade(3,"2A",2022));
+        grades.add(new Grade(4,"2B",2022));
+        grades.add(new Grade(5,"3A",2022));
+        grades.add(new Grade(6,"3B",2022));
+        grades.add(new Grade(7,"4A",2022));
+        grades.add(new Grade(8,"4B",2022));
+        grades.add(new Grade(9,"5A",2022));
+        grades.add(new Grade(10,"5B",2022));
+        grades.add(new Grade(11,"6A",2022));
+        grades.add(new Grade(12,"6B",2022));
     }
 
     public void addGrade(Grade grade){
