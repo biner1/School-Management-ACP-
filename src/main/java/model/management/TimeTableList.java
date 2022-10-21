6e3b841dd286cfe2d284ab7abcecbf5168e54f6d
abdullah-19-8 <abdullaahmadsheikh@gmail.com>
2022-10-21 20:02:57 +0300

    timeTableList fixed

--- src/main/java/model/management/TimeTableList.java
+++ src/main/java/model/management/TimeTableList.java
@@ -15,13 +15,13 @@ public class TimeTableList {
     public TimeTableList(){
         timeTables = new ArrayList<TimeTable>();
 
-        Map<String,String> map = Map.of("math","8:00", "english","9:00", "kurdish","10:00","Arabic","11:00","science","12:00","art","01:00");
+        Map<String,String> map = Map.of("english","8:00", "math","9:00", "Arabic","10:00","art","11:00","science","12:00","kurdish","01:00");
        timeTables.add(new TimeTable(1,map));
-        Map<String,String> map1 = Map.of("math","8:00", "english","9:00", "kurdish","10:00","Arabic","11:00","science","12:00","art","01:00");
+        Map<String,String> map1 = Map.of("english","8:00", "math","9:00", "Arabic","10:00","art","11:00","science","12:00","kurdish","01:00");
         timeTables.add(new TimeTable(2,map1));
-        Map<String,String> map2 = Map.of("math","8:00", "english","9:00", "kurdish","10:00","Arabic","11:00","science","12:00","art","01:00");
+        Map<String,String> map2 = Map.of("Arabic","8:00", "kurdish","9:00", "english","10:00","math","11:00","science","12:00","sport","01:00");
         timeTables.add(new TimeTable(3,map2));
-        Map<String,String> map3 = Map.of("math","8:00", "english","9:00", "kurdish","10:00","Arabic","11:00","science","12:00","art","01:00");
+        Map<String,String> map3 = Map.of("Arabic","8:00", "kurdish","9:00", "english","10:00","math","11:00","science","12:00","sport","01:00");
         timeTables.add(new TimeTable(4,map3));
         Map<String,String> map4 = Map.of("math","8:00", "english","9:00", "kurdish","10:00","Arabic","11:00","science","12:00","art","01:00");
         timeTables.add(new TimeTable(5,map4));
