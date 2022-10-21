e7a79a199629e8a18b7273db751efa631c5b6ffe
m0slah <your e-mail>
2022-10-21 23:39:37 +0300

    add StaffAttendanceList

--- src/main/java/model/management/StaffAttendanceList.java
+++ src/main/java/model/management/StaffAttendanceList.java
@@ -1,4 +1,35 @@
 package main.java.model.management;
 
+import java.util.ArrayList;
+
+import main.java.model.models.StaffAttendance;
+
 public class StaffAttendanceList {
+
+    private ArrayList<StaffAttendance> staffAttendances;
+
+    public StaffAttendanceList(){
+        staffAttendances=new ArrayList<StaffAttendance>();
+
+        staffAttendances.add(new StaffAttendance(1,1,"11/10/2020","absent"));
+        staffAttendances.add(new StaffAttendance(2,1,"14/10/2020","absent"));
+        staffAttendances.add(new StaffAttendance(3,1,"17/10/2020","absent"));
+        staffAttendances.add(new StaffAttendance(4,1,"20/10/2020","absent"));
+        staffAttendances.add(new StaffAttendance(5,2,"5/10/2020","absent"));
+        staffAttendances.add(new StaffAttendance(6,2,"8/10/2020","absent"));
+        staffAttendances.add(new StaffAttendance(7,2,"17/10/2020","absent"));
+        staffAttendances.add(new StaffAttendance(8,2,"19/10/2020","absent"));
+        staffAttendances.add(new StaffAttendance(9,3,"3/10/2020","absent"));
+        staffAttendances.add(new StaffAttendance(10,3,"16/10/2020","absent"));
+        staffAttendances.add(new StaffAttendance(11,4,"11/10/2020","absent"));
+        staffAttendances.add(new StaffAttendance(12,4,"18/10/2020","absent"));
+    }
+
+    public void addStaffAttendance(StaffAttendance staffAttendance){
+        staffAttendances.add(staffAttendance);
+    }
+
+    public ArrayList<StaffAttendance> getStaffAttendances() {
+        return staffAttendances;
+    }
 }
