bf753851ef55861a27e8444ff56e4758838aa0bc
Abdullah Ahmed ShekhMohamed <80915741+abdullah-19-8@users.noreply.github.com>
2022-10-23 03:10:23 +0300

    Filtered

--- src/main/java/model/management/StudentPaymentList.java
+++ src/main/java/model/management/StudentPaymentList.java
@@ -1,11 +1,16 @@
 package main.java.model.management;
 import main.java.model.models.StudentPayment;
 import java.util.ArrayList;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
 public class StudentPaymentList {
     private ArrayList<StudentPayment>StudentPayments;
 
     public StudentPaymentList(){
+
+        StudentPayments = new ArrayList<>();
+
         StudentPayments.add(new StudentPayment(1, 1, 10000, "4-1-2022"));
         StudentPayments.add(new StudentPayment(2, 2, 10000, "10-1-2022"));
         StudentPayments.add(new StudentPayment(3, 3, 10000, "5-1-2022"));
@@ -19,6 +24,15 @@ public class StudentPaymentList {
 
     public ArrayList<StudentPayment> getStudentPayments(){
         return StudentPayments;
+    }
+
+
+    public ArrayList<StudentPayment> getStudentPaymentByDate(String date){
+        return StudentPayments.stream().filter(g -> Objects.equals(g.getPaymentDate(), date)).collect(Collectors.toCollection(ArrayList::new));
+    }
+
+    public  ArrayList<StudentPayment> getStaffPaymentByAmount(int amount){
+        return StudentPayments.stream().filter(s -> s.getPaymentAmount() ==(amount)).collect(Collectors.toCollection(ArrayList::new));
     }
 
 }
