00946a831c3f4ed8f555a7cde9934c5a5171d23d
Zeynab <zaynabhamad950@gmail.com>
2022-10-29 23:08:38 +0300

    compleated

--- src/main/java/model/management/ExamGradeList.java
+++ src/main/java/model/management/ExamGradeList.java
@@ -5,47 +5,47 @@ import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 import main.java.model.models.ExamGrade;
-import main.java.model.models.Student;
+
 
 
 public class ExamGradeList {
 
-    private ArrayList<ExamGrade> ExamGrades;
+    private ArrayList<ExamGrade> examGrades;
 
     public ExamGradeList(){
-        ExamGrades = new ArrayList<>();
+        examGrades = new ArrayList<>();
         //TODO delete id
-        ExamGrades.add(new ExamGrade(1, 1, 78, 1));
-        ExamGrades.add(new ExamGrade(2, 1, 80, 2));
-        ExamGrades.add(new ExamGrade(3, 1, 88, 3));
-        ExamGrades.add(new ExamGrade(4, 1, 70, 4));
-        ExamGrades.add(new ExamGrade(5, 1, 90, 5));
+        examGrades.add(new ExamGrade(1, 1, 78, 1));
+        examGrades.add(new ExamGrade(2, 1, 80, 2));
+        examGrades.add(new ExamGrade(3, 1, 88, 3));
+        examGrades.add(new ExamGrade(4, 1, 70, 4));
+        examGrades.add(new ExamGrade(5, 1, 90, 5));
 
     }
 
     public void addExamGrade(ExamGrade examG){
         readFromFile();
-        ExamGrades.add(examG);
+        examGrades.add(examG);
         saveToFile();
     }
 
     public ArrayList<ExamGrade> getExamGrades(){
-        return ExamGrades;
+        return examGrades;
     }
 
     public ExamGrade getExamGradeByExamId(int examId){
         readFromFile();
-        return ExamGrades.stream().filter(g -> g.getExamId() ==examId).findAny().orElse(null);
+        return examGrades.stream().filter(g -> g.getExamId() ==examId).findAny().orElse(null);
     }
 
     public ArrayList<ExamGrade> getExamGradeByMark(int mark){
         readFromFile();
-        return ExamGrades.stream().filter(s -> s.getMark() ==(mark)).collect(Collectors.toCollection(ArrayList::new));
+        return examGrades.stream().filter(s -> s.getMark() ==(mark)).collect(Collectors.toCollection(ArrayList::new));
     }
 
     public ArrayList<ExamGrade> getExamGradeByStudentId(int studentId){
         readFromFile();
-        return ExamGrades.stream().filter(s -> s.getStudentId() ==(studentId)).collect(Collectors.toCollection(ArrayList::new));
+        return examGrades.stream().filter(s -> s.getStudentId() ==(studentId)).collect(Collectors.toCollection(ArrayList::new));
     }
 
 
@@ -55,7 +55,7 @@ public class ExamGradeList {
         try {
             FileOutputStream zed = new FileOutputStream("src/main/java/datafile/ExamGrade.txt");
             ObjectOutputStream zee = new ObjectOutputStream(zed);
-            zee.writeObject(ExamGrades);
+            zee.writeObject(examGrades);
             zee.close();
             return true;
         }
@@ -73,9 +73,9 @@ public class ExamGradeList {
         try {
             FileInputStream zed = new FileInputStream("src/main/java/datafile/ExamGrade.txt");
             ObjectInputStream zee = new ObjectInputStream(zed);
-            ExamGrades = (ArrayList<ExamGrade>) zee.readObject();
+            examGrades = (ArrayList<ExamGrade>) zee.readObject();
             zee.close();
-            return ExamGrades;
+            return examGrades;
         }catch (IOException e){
             System.out.println("error with reading file");
             return null;
--- src/main/java/model/management/ExamList.java
+++ src/main/java/model/management/ExamList.java
@@ -1,51 +1,50 @@
 package main.java.model.management;
 
 import main.java.model.models.Exam;
-import main.java.model.models.Student;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 public class ExamList{
-    private ArrayList<Exam> Exams;
+    private ArrayList<Exam> exams;
 
     public ExamList(){
-        Exams = new ArrayList<>();
+        exams = new ArrayList<>();
 
         //TODO delete examId
-        Exams.add(new Exam(1, 1 , "informatics", "1-2-2022"));
-        Exams.add(new Exam(2, 2 , "Software", "2-2-2022"));
-        Exams.add(new Exam(3, 3 , "Math", "3-2-2022"));
-        Exams.add(new Exam(4, 4 , "Analysis", "4-2-2022"));
-        Exams.add(new Exam(5, 5 , "AI", "5-2-2022"));
+        exams.add(new Exam(1, 1 , "informatics", "1-2-2022"));
+        exams.add(new Exam(2, 2 , "Software", "2-2-2022"));
+        exams.add(new Exam(3, 3 , "Math", "3-2-2022"));
+        exams.add(new Exam(4, 4 , "Analysis", "4-2-2022"));
+        exams.add(new Exam(5, 5 , "AI", "5-2-2022"));
 
     }
 
     public void addExam(Exam exam){
         readFromFile();
-        Exams.add(exam);
+        exams.add(exam);
         saveToFile();
     }
 
     public ArrayList<Exam> getExams(){
-        return Exams;
+        return exams;
     }
 
 
     public  ArrayList<Exam> getExamByName(String name){
         readFromFile();
-        return Exams.stream().filter(s -> s.getExamDesc().toLowerCase().equals(name.toLowerCase())).collect(Collectors.toCollection(ArrayList::new));
+        return exams.stream().filter(s -> s.getExamDesc().toLowerCase().equals(name.toLowerCase())).collect(Collectors.toCollection(ArrayList::new));
     }
 
     public ArrayList<Exam> getExamGradeBySubjectId(int id){
         readFromFile();
-        return Exams.stream().filter(s -> s.getSubjectId() ==(id)).collect(Collectors.toCollection(ArrayList::new));
+        return exams.stream().filter(s -> s.getSubjectId() ==(id)).collect(Collectors.toCollection(ArrayList::new));
     }
 
     public  ArrayList<Exam> getExamGradeByDate(String date){
         readFromFile();
-        return Exams.stream().filter(s -> s.getDate().toLowerCase().equals(date.toLowerCase())).collect(Collectors.toCollection(ArrayList::new));
+        return exams.stream().filter(s -> s.getDate().toLowerCase().equals(date.toLowerCase())).collect(Collectors.toCollection(ArrayList::new));
     }
 
 
@@ -54,7 +53,7 @@ public class ExamList{
         try {
             FileOutputStream zed = new FileOutputStream("src/main/java/datafile/Exams.txt");
             ObjectOutputStream zee = new ObjectOutputStream(zed);
-            zee.writeObject(Exams);
+            zee.writeObject(exams);
             zee.close();
             return true;
         }
@@ -71,9 +70,9 @@ public class ExamList{
         try {
             FileInputStream zed = new FileInputStream("src/main/java/datafile/Exams.txt");
             ObjectInputStream zee = new ObjectInputStream(zed);
-            Exams = (ArrayList<Exam>) zee.readObject();
+            exams = (ArrayList<Exam>) zee.readObject();
             zee.close();
-            return Exams;
+            return exams;
         }catch (IOException e){
             System.out.println("error with reading file");
             return null;
