b373db34e87e1e2a06cb4ba632d3e7877a9fb9ec
Zeynab <zaynabhamad950@gmail.com>
2022-10-29 22:54:35 +0300

    compleated

--- src/main/java/model/management/ExamGradeList.java
+++ src/main/java/model/management/ExamGradeList.java
@@ -1,9 +1,11 @@
 package main.java.model.management;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
 import main.java.model.models.ExamGrade;
+import main.java.model.models.Student;
 
 
 public class ExamGradeList {
@@ -22,8 +24,9 @@ public class ExamGradeList {
     }
 
     public void addExamGrade(ExamGrade examG){
-
+        readFromFile();
         ExamGrades.add(examG);
+        saveToFile();
     }
 
     public ArrayList<ExamGrade> getExamGrades(){
@@ -31,15 +34,56 @@ public class ExamGradeList {
     }
 
     public ExamGrade getExamGradeByExamId(int examId){
+        readFromFile();
         return ExamGrades.stream().filter(g -> g.getExamId() ==examId).findAny().orElse(null);
     }
 
     public ArrayList<ExamGrade> getExamGradeByMark(int mark){
+        readFromFile();
         return ExamGrades.stream().filter(s -> s.getMark() ==(mark)).collect(Collectors.toCollection(ArrayList::new));
     }
 
     public ArrayList<ExamGrade> getExamGradeByStudentId(int studentId){
+        readFromFile();
         return ExamGrades.stream().filter(s -> s.getStudentId() ==(studentId)).collect(Collectors.toCollection(ArrayList::new));
+    }
+
+
+
+
+    public boolean saveToFile(){
+        try {
+            FileOutputStream zed = new FileOutputStream("src/main/java/datafile/ExamGrade.txt");
+            ObjectOutputStream zee = new ObjectOutputStream(zed);
+            zee.writeObject(ExamGrades);
+            zee.close();
+            return true;
+        }
+        catch (IOException e){
+            System.out.println("file output error");
+            return false;
+        }
+    }
+
+
+
+
+
+    public ArrayList<ExamGrade> readFromFile(){
+        try {
+            FileInputStream zed = new FileInputStream("src/main/java/datafile/ExamGrade.txt");
+            ObjectInputStream zee = new ObjectInputStream(zed);
+            ExamGrades = (ArrayList<ExamGrade>) zee.readObject();
+            zee.close();
+            return ExamGrades;
+        }catch (IOException e){
+            System.out.println("error with reading file");
+            return null;
+        }
+        catch(ClassNotFoundException e){
+            System.out.println("ClassNotFoundException");
+            return null;
+        }
     }
 
 
--- src/main/java/model/management/ExamList.java
+++ src/main/java/model/management/ExamList.java
@@ -1,7 +1,9 @@
 package main.java.model.management;
 
 import main.java.model.models.Exam;
+import main.java.model.models.Student;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.stream.Collectors;
 
@@ -21,8 +23,9 @@ public class ExamList{
     }
 
     public void addExam(Exam exam){
-
+        readFromFile();
         Exams.add(exam);
+        saveToFile();
     }
 
     public ArrayList<Exam> getExams(){
@@ -31,15 +34,54 @@ public class ExamList{
 
 
     public  ArrayList<Exam> getExamByName(String name){
+        readFromFile();
         return Exams.stream().filter(s -> s.getExamDesc().toLowerCase().equals(name.toLowerCase())).collect(Collectors.toCollection(ArrayList::new));
     }
 
     public ArrayList<Exam> getExamGradeBySubjectId(int id){
+        readFromFile();
         return Exams.stream().filter(s -> s.getSubjectId() ==(id)).collect(Collectors.toCollection(ArrayList::new));
     }
 
     public  ArrayList<Exam> getExamGradeByDate(String date){
+        readFromFile();
         return Exams.stream().filter(s -> s.getDate().toLowerCase().equals(date.toLowerCase())).collect(Collectors.toCollection(ArrayList::new));
+    }
+
+
+
+    public boolean saveToFile(){
+        try {
+            FileOutputStream zed = new FileOutputStream("src/main/java/datafile/Exams.txt");
+            ObjectOutputStream zee = new ObjectOutputStream(zed);
+            zee.writeObject(Exams);
+            zee.close();
+            return true;
+        }
+        catch (IOException e){
+            System.out.println("file output error");
+            return false;
+        }
+    }
+
+
+
+
+    public ArrayList<Exam> readFromFile(){
+        try {
+            FileInputStream zed = new FileInputStream("src/main/java/datafile/Exams.txt");
+            ObjectInputStream zee = new ObjectInputStream(zed);
+            Exams = (ArrayList<Exam>) zee.readObject();
+            zee.close();
+            return Exams;
+        }catch (IOException e){
+            System.out.println("error with reading file");
+            return null;
+        }
+        catch(ClassNotFoundException e){
+            System.out.println("ClassNotFoundException");
+            return null;
+        }
     }
 
 
